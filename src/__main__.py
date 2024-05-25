45af8dc63cfae3cea5dc2f47c8aa0a5749ff389c
qiisqwww <qiisqwww@gmail.com>
2024-05-25 20:57:25 +0300

    fix

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,14 @@
 import asyncio
 
+from loguru import logger
+
 from src.app_config import configurate_logger
 
 
 async def main() -> None:
     configurate_logger()  # Configuration of the logger
+    logger.info("LOGGER CONFIGURED")
+
 
 if __name__ == '__main__':
     asyncio.run(main())
