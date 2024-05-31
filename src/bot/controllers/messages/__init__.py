2a0c2e8b88102dbb3c3f326faa96416dfe47f05f
qiisqwww <qiisqwww@gmail.com>
2024-05-31 12:47:20 +0300

    fixed bygs

--- src/bot/controllers/messages/__init__.py
+++ src/bot/controllers/messages/__init__.py
@@ -1,6 +1,6 @@
 from .void import include_void_command_router
 from .start import include_start_command_router
-from .photo import include_covert_image_router
+from .convert_image import include_covert_image_router
 
 __all__ = [
     "include_covert_image_router",
